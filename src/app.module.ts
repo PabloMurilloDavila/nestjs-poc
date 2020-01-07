a87316615ae5ef40dfaa5c70a274fc39e7b68ce4
DanieldelaPena <daniel.delapena@thecocktail.com>
2020-01-07 09:04:22 +0100

    mySQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
-
+import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { Cat } from './cats/cat.entity'
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    CatsModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
