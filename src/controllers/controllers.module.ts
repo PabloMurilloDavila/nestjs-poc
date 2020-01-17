ba37a8560f9e961d758086bd925615b10e4eae0e
DanieldelaPena <daniel.delapena@thecocktail.com>
2020-01-17 12:59:10 +0100

    No dogs module

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule  } from '@nestjs/common';
 import { DogsController } from './dogs.controller';
-import {DogsModule} from '../services/dogs/dogs.module';
 import {DogsService} from '../services/dogs/dogs.service'
 
 @Module({
     controllers: [
         DogsController,
       ],
-      imports: [DogsModule],
+      imports: [HttpModule.register({
+        timeout: 5000,
+        maxRedirects: 5,
+      }),],
       providers: [DogsService],
 })
 export class ControllersModule {}
