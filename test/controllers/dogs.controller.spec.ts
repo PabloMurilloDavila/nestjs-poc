2b2aca33323d4fdb0b0221c20815d176cfc36ea8
DanieldelaPena <daniel.delapena@thecocktail.com>
2020-01-20 10:08:06 +0100

    Tests working

--- test/controllers/dogs.controller.spec.ts
+++ test/controllers/dogs.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DogsController } from '../../src/controllers/dogs.controller';
+import { DogsService } from '../../src/services/dogs/dogs.service';
 
 describe('Dogs Controller', () => {
   let controller: DogsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [DogsController],
+      controllers: [DogsController], providers: [DogsService]
     }).compile();
 
     controller = module.get<DogsController>(DogsController);
