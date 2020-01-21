7fb30128eed6b679a61a730ab724f5714b0d1e8a
DanieldelaPena <daniel.delapena@thecocktail.com>
2020-01-21 09:41:34 +0100

    All use cases / .env.example

--- src/controllers/dogs.controller.ts
+++ src/controllers/dogs.controller.ts
@@ -1,36 +1,45 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { GetDogs } from '../core/use_cases/dogs/get.dogs'
+import { GetAllBreeds } from '../core/use_cases/dogs/get.all.breeds';
+import { GetSubbreedsList } from '../core/use_cases/dogs/get.subreeds.list';
+import { GetRandomImage } from '../core/use_cases/dogs/get.random.image';
+import { GetRandomBreedImage } from '../core/use_cases/dogs/get.random.breed.image';
+import { GetBreedImages } from '../core/use_cases/dogs/get.breed.images';
+
 
 @Controller('dogs')
 export class DogsController {
 
   constructor(
-    private readonly getDogs: GetDogs,
+    private readonly getAllBreeds: GetAllBreeds,
+    private readonly getSubbreedList: GetSubbreedsList,
+    private readonly getRandomImage: GetRandomImage,
+    private readonly getRandomBreedImage: GetRandomBreedImage,
+    private readonly getBreedImages: GetBreedImages,
   ) { }
 
   @Get()
   getallBreeds() {
-    return this.getDogs.all_Breeds();
+    return this.getAllBreeds.call();
   }
 
   @Get('/:breed')
   getSubBreedList(@Param('breed') breed: string) {
-    return this.getDogs.subBreed_List(breed);
+    return this.getSubbreedList.call(breed);
   }
 
   @Get('/img/rnd')
-  getRandomImage() {
-    return this.getDogs.random_Image();
+  getRndImage() {
+    return this.getRandomImage.call();
   }
 
   @Get('/img/rnd/:breed')
-  getRandomBreedImage(@Param('breed') breed: string) {
-    return this.getDogs.randomBreed_Image(breed);
+  getRndBreedImage(@Param('breed') breed: string) {
+    return this.getRandomBreedImage.call(breed);
   }
 
   @Get('/img/:breed')
-  getBreedImages(@Param('breed') breed: string) {
-    return this.getDogs.randomBreed_Image(breed);
+  getBreedImgs(@Param('breed') breed: string) {
+    return this.getBreedImages.call(breed);
   }
 
 }
--- src/core/use_cases/dogs/dogs.module.ts
+++ src/core/use_cases/dogs/dogs.module.ts
@@ -1,12 +1,19 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { GetDogs } from './get.dogs';
+
+import { GetAllBreeds } from './get.all.breeds';
+import { GetSubbreedsList } from './get.subreeds.list';
+import { GetRandomImage } from './get.random.image';
+import { GetRandomBreedImage } from './get.random.breed.image';
+import { GetBreedImages } from './get.breed.images';
 import { DogsService } from '../../../services/dogs/dogs.service';
+
+
 @Module({
-    providers: [GetDogs, DogsService],
+    providers: [GetAllBreeds, GetSubbreedsList, DogsService, GetRandomImage, GetRandomBreedImage, GetBreedImages],
     imports: [HttpModule.register({
         timeout: 5000,
         maxRedirects: 5,
     }),],
-    exports: [GetDogs],
+    exports: [GetAllBreeds, GetSubbreedsList, GetRandomImage, GetRandomBreedImage, GetBreedImages],
 })
 export class DogsModule { }
