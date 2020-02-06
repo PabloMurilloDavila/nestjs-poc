41d1923dac5ae3fdd389c2be70a1470c9de5ec3f
DanieldelaPena <daniel.delapena@thecocktail.com>
2020-02-06 09:08:57 +0100

    Custom error

---
+++ src/core/errors.ts
@@ -0,0 +1,36 @@
+import { ApiErrorDto } from './dto/api.error.dto';
+
+export const ApiErrors = {
+  UNKNOWN_API_ERROR: {
+    code: 1003,
+    message: 'Something has failed.',
+  },
+};
+
+export const DomainErrors = {
+  ...ApiErrors,
+  INVALID_CAT_ID: {
+    code: 3001,
+    message: 'Invalid cat id provided.',
+  },
+};
+
+export interface IDomainError {
+  code: number;
+  message: string;
+  extraInfo?: object;
+}
+
+export class DomainError extends Error {
+  constructor(private readonly error: IDomainError) {
+    super(error.message);
+  }
+
+  public toApiError(): ApiErrorDto {
+    return this.error;
+  }
+
+  public static getError(errorCode: any): IDomainError {
+    return DomainErrors[errorCode] || DomainErrors.UNKNOWN_API_ERROR;
+  }
+}
